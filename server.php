e7dbde53b049638f2b2a0b3c15d43d449b16b44e
jucetina <juliethcetina@gmail.com>
2021-03-12 20:39:36 -0500

    Se intenta corregir el error de CORS 2

--- server.php
+++ server.php
@@ -136,7 +136,10 @@ if(!in_array($resource, $allowedResources)){
 $resource_id = array_key_exists('resource_id', $_GET) ? $_GET['resource_id'] : '';
 
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE');
+header("Access-Control-Allow-Headers: X-Requested-With");
 
 switch($_SERVER['REQUEST_METHOD']){
     case 'GET':
