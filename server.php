b29280a187933b5e19ddb8f04e09dc6c02af8587
jucetina <juliethcetina@gmail.com>
2021-03-13 16:12:25 -0500

    Modificando server.php verbo PUT

--- server.php
+++ server.php
@@ -173,6 +173,7 @@ switch($_SERVER['REQUEST_METHOD']){
             if(!empty($key) || $key === 0){
                 $json = file_get_contents('php://input');
                 $badges[ $key ] = json_decode($json, true);
+                $badges[ $key ]['id'] = $resource_id;
                 echo json_encode($badges);
             }else{
 
