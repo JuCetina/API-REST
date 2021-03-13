ab15b749039b72b67a9c770dd85fc4d8ea46f096
jucetina <juliethcetina@gmail.com>
2021-03-12 22:13:52 -0500

    Se intenta corregir el error de CORS 12

--- server.php
+++ server.php
@@ -138,7 +138,6 @@ $resource_id = array_key_exists('resource_id', $_GET) ? $_GET['resource_id'] : '
 
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 
 
 
