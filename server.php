5d95f41b7a5a5fe48f3e31c9fff3cea60b7ac34f
jucetina <juliethcetina@gmail.com>
2021-03-13 15:23:00 -0500

    Modificando server.php para utilizar el API con el proyecto de react.js

--- server.php
+++ server.php
@@ -78,7 +78,7 @@ $allowedResources = [
 ];
 
 $badges = [
-    1 => [
+    [
         "id" => "1",
         "firstName" => "Zoie",
         "lastName"  => "Grant",
@@ -87,7 +87,7 @@ $badges = [
         "twitter" => "ZoieGrant05512-2019",
         "avatarUrl" => "https://www.gravatar.com/avatar/0fc6d5ee2ee176d4581acf6a7e5644cc?d=identicon",
     ],
-    2 => [
+    [
         "id" => "2",
         "firstName" => "Dustin",
         "lastName" => "Stehr",
@@ -96,7 +96,7 @@ $badges = [
         "twitter" => "DustinStehr77585-9157",
         "avatarUrl" => "https://picsum.photos/80",
     ],
-    3 => [
+    [
         "id" => "3",
         "firstName" => "Karlee",
         "lastName" => "Satterfield",
@@ -105,7 +105,7 @@ $badges = [
         "twitter" => "KarleeSatterfield32692-9732",
         "avatarUrl" => "https://www.gravatar.com/avatar/c2d679f9b44e1869548ab95aac18b7c9?d=identicon",
     ],
-    4 => [
+    [
         "id" => "4",
         "firstName" => "Ernie",
         "lastName" => "Schmidt",
@@ -114,7 +114,7 @@ $badges = [
         "twitter" => "ErnieSchmidt56445-6854",
         "avatarUrl" => "https://www.gravatar.com/avatar/6a7e6f60ad63c102322894ab94a26f2f?d=identicon",
     ],
-    5 => [
+    [
         "id" => "5",
         "firstName" => "Kelly",
         "lastName" => "Corkery",
@@ -150,10 +150,15 @@ switch($_SERVER['REQUEST_METHOD']){
     case 'GET':
         if(empty($resource_id)){
             echo json_encode($badges);
-        }elseif(array_key_exists($resource_id, $badges)) {
-            echo json_encode($badges[ $resource_id ]);
+        // }elseif(array_key_exists($resource_id, $badges)) {
         }else{
-            http_response_code( 404 );
+            $key = array_search($resource_id, array_column($badges, 'id'));
+            if(!empty($key) || $key === 0){
+                echo json_encode($badges[ $key ]);
+            }else{
+
+                http_response_code( 404 );
+            }
         }
         break;
     case 'POST':
@@ -162,16 +167,30 @@ switch($_SERVER['REQUEST_METHOD']){
         echo json_encode($badges);
         break;
     case 'PUT':
-        if(!empty($resource_id) && array_key_exists($resource_id, $badges)){
-            $json = file_get_contents('php://input');
-            $badges[ $resource_id ] = json_decode($json, true);
-            echo json_encode($badges);
+        // if(!empty($resource_id) && array_key_exists($resource_id, $badges)){
+        if(!empty($resource_id)){
+            $key = array_search($resource_id, array_column($badges, 'id'));
+            if(!empty($key) || $key === 0){
+                $json = file_get_contents('php://input');
+                $badges[ $key ] = json_decode($json, true);
+                echo json_encode($badges);
+            }else{
+
+                http_response_code( 404 );
+            }
         }
         break;
     case 'DELETE':
-        if(!empty($resource_id) && array_key_exists($resource_id, $badges)){
-            unset($badges[ $resource_id ]);
+        // if(!empty($resource_id) && array_key_exists($resource_id, $badges)){
+        if(!empty($resource_id)){
+            $key = array_search($resource_id, array_column($badges, 'id'));
+            if(!empty($key) || $key === 0){
+                unset($badges[ $key]);
+                echo json_encode($badges);
+            }else{
+
+                http_response_code( 404 );
+            }
         }
-        echo json_encode($badges);
         break;
 }
