dd2c04f0935460242a77f473d39953e83d2bc39e
jucetina <juliethcetina@gmail.com>
2021-03-12 22:00:25 -0500

    Se intenta corregir el error de CORS 8

--- server.php
+++ server.php
@@ -137,14 +137,7 @@ $resource_id = array_key_exists('resource_id', $_GET) ? $_GET['resource_id'] : '
 
 
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-header("Allow: GET, POST, OPTIONS, PUT, DELETE");
-$method = $_SERVER['REQUEST_METHOD'];
-if($method == "OPTIONS") {
-    die();
-}
+
 
 
 switch($_SERVER['REQUEST_METHOD']){
