d5f6d68e02688a261a006032e4c3797d21bef892
jucetina <juliethcetina@gmail.com>
2021-03-12 20:41:57 -0500

    Se intenta corregir el error de CORS 3

--- server.php
+++ server.php
@@ -136,10 +136,7 @@ if(!in_array($resource, $allowedResources)){
 $resource_id = array_key_exists('resource_id', $_GET) ? $_GET['resource_id'] : '';
 
 header('Content-Type: application/json');
-header("Access-Control-Allow-Origin: *");
-header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE');
-header("Access-Control-Allow-Headers: X-Requested-With");
+header('Access-Control-Allow-Origin: *');
 
 switch($_SERVER['REQUEST_METHOD']){
     case 'GET':
